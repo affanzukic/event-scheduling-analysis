b6c833b694ff58e7363d7e40f6590ca355d6d02f
Afan Zukić <affan.zukic@icloud.com>
2025-08-26 14:56:44 +0200

    feat: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next', 'prettier'],
+  }),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+
+      indent: 'off',
+      '@typescript-eslint/indent': 'off',
+
+      'no-unused-vars': 'off',
+
+      'no-empty': ['error', { allowEmptyCatch: true }],
+    },
   },
 ];
 
